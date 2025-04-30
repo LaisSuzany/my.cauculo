8e89a095caf9373d8e85ce9d77972ab71a517805
victormarcelo268 <victormarcelo268@gmail.com>
2025-04-30 16:49:08 -0300

    Create Soma.py

---
+++ Soma.py
@@ -0,0 +1,9 @@
+def somar(a, b):
+    return a + b
+
+# Exemplo de uso
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+resultado = somar(num1, num2)
+print("Resultado da soma:", resultado)
