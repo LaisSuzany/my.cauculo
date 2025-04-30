a09ae30d9f411da315a5c34115a9224c8f44cdf6
victormarcelo268 <victormarcelo268@gmail.com>
2025-04-30 01:16:15 -0300

    Create Multiplicação.py

---
+++ Multiplicação.py
@@ -0,0 +1,9 @@
+def multiplicar(a, b):
+    return a * b
+
+# Exemplo de uso
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+resultado = multiplicar(num1, num2)
+print(f"O resultado da multiplicação é: {resultado}")
