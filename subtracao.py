9f7abacb0e0ea25bcb629944d8c7cd014417b7a3
Ash <ashtuni27@gmail.com>
2025-04-30 00:14:40 -0300

    Ash adicionou a função de subtração

---
+++ subtracao.py
@@ -0,0 +1,9 @@
+def subtrair(a, b):
+    return a - b
+
+# Teste
+if __name__ == "__main__":
+    x = float(input("Digite o primeiro número: "))
+    y = float(input("Digite o segundo número: "))
+    print("Resultado da subtração:", subtrair(x, y))
+
