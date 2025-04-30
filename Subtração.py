141a2feb71fc591fbfeed767987e095220f425a6
victormarcelo268 <victormarcelo268@gmail.com>
2025-04-30 16:47:36 -0300

    Create Subtração.py

---
+++ Subtração.py
@@ -0,0 +1,9 @@
+def subtrair(a, b):
+    return a - b
+
+# Exemplo de uso
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+resultado = subtrair(num1, num2)
+print("Resultado da subtração:", resultado)
