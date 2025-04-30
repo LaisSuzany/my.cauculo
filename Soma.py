550ef65e15b3e887dd367a297edbc7e6071bee75
victormarcelo268 <victormarcelo268@gmail.com>
2025-04-29 23:51:02 -0300

    Create Soma.py

---
+++ Soma.py
@@ -0,0 +1,11 @@
+# Calculadora de Soma em Python
+
+# Solicita dois números ao usuário
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Realiza a soma
+soma = numero1 + numero2
+
+# Exibe o resultado
+print("A soma é:", soma)
